fc7c7bf95faaffa91e9f77c77a27c1c97aec69b8
Surisetti Dinesh <mingolaport@gmail.com>
2020-02-07 17:15:22 +0530

    commit

---
+++ index.js
@@ -0,0 +1,99 @@
+const express = require('express'),
+     http = require('http');
+const {c, cpp, node, python, java} = require('compile-run');
+const bodyParser = require('body-parser');
+const hostname = 'localhost';
+const app = express();
+port = process.env.PORT || 3000;
+
+app.set('view engine','ejs');
+app.use(bodyParser.json());      
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/node_modules'));
+app.use(express.static(__dirname + '/Public'));
+
+app.get('/',(req,res) => {
+	res.sendFile(__dirname+'/Public/index.html');
+});
+
+app.post('/compile/:feedsId',(req,res) => {
+	if(req.params.feedsId=="python2"){
+		let resultPromise = python.runSource(req.body.carrier, {stdin : req.body.carrier_ip});
+		resultPromise.then(result => {
+			if(result.stderr){
+				var n=result.stderr.search("-")+1;
+				result.stderr=result.stderr.substring(n);
+			}
+			console.log(result);
+			res.end(JSON.stringify({ result_output: result }));
+		})
+		.catch(err => {
+			console.log(err);
+		});
+	}
+	else if(req.params.feedsId=="python3"){
+		let resultPromise = python.runSource(req.body.carrier, {executionPath: 'python3', stdin : req.body.carrier_ip});
+		resultPromise.then(result => {
+			if(result.stderr){
+				var n=result.stderr.search("-")+1;
+				result.stderr=result.stderr.substring(n);
+			}
+			console.log(result);
+			res.end(JSON.stringify({ result_output: result }));
+		})
+		.catch(err => {
+			console.log(err);
+		});
+	}
+	else if(req.params.feedsId=="cpp"){
+		let resultPromise = cpp.runSource(req.body.carrier, {stdin : req.body.carrier_ip});
+		resultPromise.then(result => {
+			if(result.stderr){
+				var n=result.stderr.search("-")+1;
+				result.stderr=result.stderr.substring(n);
+			}
+			console.log(result);
+			res.end(JSON.stringify({ result_output: result }));
+		})
+		.catch(err => {
+			console.log(err);
+		});
+	}
+	else if(req.params.feedsId=="c"){
+		let resultPromise = c.runSource(req.body.carrier, {stdin : req.body.carrier_ip});
+		resultPromise.then(result => {
+			if(result.stderr){
+				var n=result.stderr.search("-")+1;
+				result.stderr=result.stderr.substring(n);
+			}
+			console.log(result);
+			res.end(JSON.stringify({ result_output: result }));
+		})
+		.catch(err => {
+			console.log(err);
+		});
+	}
+	else if(req.params.feedsId=="java"){
+		let resultPromise = java.runSource(req.body.carrier, {stdin : req.body.carrier_ip});
+		resultPromise.then(result => {
+			if(result.stderr){
+				var n=result.stderr.search("-")+1;
+				result.stderr=result.stderr.substring(n);
+			}
+			console.log(result);
+			res.end(JSON.stringify({ result_output: result }));
+		})
+		.catch(err => {
+			console.log(err);
+		});
+	}
+	else{
+		res.end("Wrong Selection");
+	}
+});
+
+const server = http.createServer(app);
+
+server.listen(port, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
