49a56371ca322be32a0d5746d81201385bcc1e59
Surisetti Dinesh <mingolaport@gmail.com>
2020-02-14 23:47:29 +0530

    Trial Commit 16

--- index.js
+++ index.js
@@ -23,11 +23,11 @@ port = process.env.PORT || 3000;
 
 var httpsServer = https.createServer(app);
 
-var server = http.createServer(app).listen(port, () => {
+var server = https.createServer(app).listen(port, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
-var peer = ExpressPeerServer(httpsServer.listen(443), options);
+var peer = ExpressPeerServer(server, options);
 
 
 app.set('view engine','ejs');
