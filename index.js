695b9e84607c6c125c5a342dbd4291bf50ba1e54
Surisetti Dinesh <mingolaport@gmail.com>
2020-02-18 22:11:00 +0530

    commit 1

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/node_modules'));
 app.use(express.static(__dirname + '/Public'));
-app.use(express.static(__dirname + '/Views'));
+app.use(express.static(__dirname + '/views'));
 
 //var httpsServer = https.createServer(app);
 
